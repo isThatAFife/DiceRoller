3360033043a98b19acb2833c5e5b369c938f74c2
Dylan Knapp <knappd@tcd.ie>
2024-01-27 06:38:37 +0000

    chore: finished updating dice

--- assets/js/roller.js
+++ assets/js/roller.js
@@ -14,34 +14,34 @@ d6.addEventListener('click', function() {
     sixAnswer.innerHTML = `You rolled a ${sixResult}!`
 });
 
-let d8 = document.getElementById('dfour');
+let d8 = document.getElementById('d8');
 
-d4.addEventListener('click', function() {
-    let fourResult = Math.floor(Math.random() * 4 + 1);
-    let fourAnswer = document.getElementById('fourResult')
-    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+d8.addEventListener('click', function() {
+    let eightResult = Math.floor(Math.random() * 8 + 1);
+    let eightAnswer = document.getElementById('eightResult')
+    eightAnswer.innerHTML = `You rolled a ${eightResult}!`
 });
 
-let d10 = document.getElementById('dfour');
+let d10 = document.getElementById('d10');
 
-d4.addEventListener('click', function() {
-    let fourResult = Math.floor(Math.random() * 4 + 1);
-    let fourAnswer = document.getElementById('fourResult')
-    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+d10.addEventListener('click', function() {
+    let tenResult = Math.floor(Math.random() * 10 + 1);
+    let tenAnswer = document.getElementById('tenResult')
+    tenAnswer.innerHTML = `You rolled a ${tenResult}!`
 });
 
-let d12 = document.getElementById('dfour');
+let d12 = document.getElementById('d12');
 
-d4.addEventListener('click', function() {
-    let fourResult = Math.floor(Math.random() * 4 + 1);
-    let fourAnswer = document.getElementById('fourResult')
-    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+d12.addEventListener('click', function() {
+    let twelveResult = Math.floor(Math.random() * 12 + 1);
+    let twelveAnswer = document.getElementById('twelveResult')
+    twelveAnswer.innerHTML = `You rolled a ${twelveResult}!`
 });
 
-let d20 = document.getElementById('dfour');
+let d20 = document.getElementById('d20');
 
-d4.addEventListener('click', function() {
-    let fourResult = Math.floor(Math.random() * 4 + 1);
-    let fourAnswer = document.getElementById('fourResult')
-    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+d20.addEventListener('click', function() {
+    let twentyResult = Math.floor(Math.random() * 20 + 1);
+    let twentyAnswer = document.getElementById('twentyResult')
+    twentyAnswer.innerHTML = `You rolled a ${twentyResult}!`
 });
