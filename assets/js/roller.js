9e5a7b108789cfac807152e25e4850787e98297b
Dylan Knapp <knappd@tcd.ie>
2024-01-27 06:34:52 +0000

    feat: got basic JS in place, just need to update for each die

---
+++ assets/js/roller.js
@@ -0,0 +1,47 @@
+let d4 = document.getElementById('d4');
+
+d4.addEventListener('click', function() {
+    let fourResult = Math.floor(Math.random() * 4 + 1);
+    let fourAnswer = document.getElementById('fourResult')
+    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+});
+
+let d6 = document.getElementById('d6');
+
+d6.addEventListener('click', function() {
+    let sixResult = Math.floor(Math.random() * 6 + 1);
+    let sixAnswer = document.getElementById('sixResult')
+    sixAnswer.innerHTML = `You rolled a ${sixResult}!`
+});
+
+let d8 = document.getElementById('dfour');
+
+d4.addEventListener('click', function() {
+    let fourResult = Math.floor(Math.random() * 4 + 1);
+    let fourAnswer = document.getElementById('fourResult')
+    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+});
+
+let d10 = document.getElementById('dfour');
+
+d4.addEventListener('click', function() {
+    let fourResult = Math.floor(Math.random() * 4 + 1);
+    let fourAnswer = document.getElementById('fourResult')
+    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+});
+
+let d12 = document.getElementById('dfour');
+
+d4.addEventListener('click', function() {
+    let fourResult = Math.floor(Math.random() * 4 + 1);
+    let fourAnswer = document.getElementById('fourResult')
+    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+});
+
+let d20 = document.getElementById('dfour');
+
+d4.addEventListener('click', function() {
+    let fourResult = Math.floor(Math.random() * 4 + 1);
+    let fourAnswer = document.getElementById('fourResult')
+    fourAnswer.innerHTML = `You rolled a ${fourResult}!`
+});
